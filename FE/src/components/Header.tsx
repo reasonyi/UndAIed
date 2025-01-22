d9fb009f854f400a4dd369a411a25f8a4daae9af
imjh990614 <imjh990614@gmail.com>
2025-01-22 23:41:45 +0900

    feat: header 초안 및 props 설정

--- FE/src/components/Header.tsx
+++ FE/src/components/Header.tsx
@@ -1,43 +1,67 @@
-import { Link, useNavigate } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom";
+import React, { useState } from "react";
 
-function Header(){
-    const navigate = useNavigate()
-    const onBoardClick = () => {
-        navigate("/board")
-    }
-    return (
-        <header>
-            <ul className="flex">
-                <li className="mr-6">
-                    <Link to={"/"}>Home으로</Link>
-                </li>
-                <li className="mr-6">
-                    <Link to={"/policy"}>Policy로</Link>
-                </li>
-                <li className="mr-6">
-                    <Link to={"/gamerooms"}>Game rooms으로</Link>
-                </li>
-                <li className="mr-6">
-                    <Link to={"/gamelobby/1"}>1번 gamelobby로</Link>
-                </li>
-                <li className="mr-6">
-                    <Link to={"/gamechats/2"}>2번 game chats로</Link>
-                </li>
-                <li className="mr-6">
-                    <Link to={"/user/3"}>3번 user로</Link>
-                </li>
-                <li className="mr-6">
-                    <Link to={"/log/4"}>4번 log로</Link>
-                </li>
-                <li className="mr-6">
-                    <button onClick={onBoardClick}>board로</button>
-                </li>
-                <li className="mr-6">
-                    <Link to={"/write"}>게시글작성으로</Link>
-                </li>
-            </ul>
-        </header>
-    )
+interface IHeaderProps {
+  scrollRatio: number;
 }
+
+const Header: React.FC<IHeaderProps> = ({ scrollRatio }) => {
+  const [scrollTop, setScrollTop] = useState(0);
+
+  const handleScroll = (event: React.UIEvent<HTMLDivElement>) => {
+    const currentScrollTop = event.currentTarget.scrollTop;
+    setScrollTop(currentScrollTop);
+    console.log(scrollTop);
+  };
+
+  const navigate = useNavigate();
+  const onBoardClick = () => {
+    navigate("/board");
+  };
+
+  return (
+    <header
+      onScroll={handleScroll}
+      className="flex flex-col header-style w-screen h-14 fixed primary-bg-black"
+    >
+      <div></div>
+      <ul className="flex ">
+        <li className="mr-6">
+          <Link to={"/"}>Home으로</Link>
+        </li>
+        <li className="mr-6">
+          <Link to={"/policy"}>Policy로</Link>
+        </li>
+        <li className="mr-6">
+          <Link to={"/gamerooms"}>Game rooms으로</Link>
+        </li>
+        <li className="mr-6">
+          <Link to={"/gamelobby/1"}>1번 gamelobby로</Link>
+        </li>
+        <li className="mr-6">
+          <Link to={"/gamechats/2"}>2번 game chats로</Link>
+        </li>
+        <li className="mr-6">
+          <Link to={"/user/3"}>3번 user로</Link>
+        </li>
+        <li className="mr-6">
+          <Link to={"/log/4"}>4번 log로</Link>
+        </li>
+        <li className="mr-6">
+          <button onClick={onBoardClick}>board로</button>
+        </li>
+        <li className="mr-6">
+          <Link to={"/write"}>게시글작성으로</Link>
+        </li>
+      </ul>
+      <div
+        className="header-bottom-style"
+        style={{
+          background: `linear-gradient(to right, #eee, #eee ${scrollRatio}%, #333 ${scrollRatio}%)`,
+        }}
+      ></div>
+    </header>
+  );
+};
 
 export default Header;
--- FE/src/pages/Home.tsx
+++ FE/src/pages/Home.tsx
@@ -1,20 +1,43 @@
+import { useState, useEffect } from "react";
+
 import { useOutletContext } from "react-router-dom";
 import LoggedInHome from "../components/LoggedInHome";
 import LoggedOutHome from "../components/LoggedOutHome";
+import Header from "../components/Header";
+import HeaderTemp from "../components/HeaderTemp";
 
 interface OuletContextType {
-    isLoggedIn: boolean,
+  isLoggedIn: boolean;
 }
 
-function Home(){
-    const {isLoggedIn} = useOutletContext<OuletContextType>();
+function Home() {
+  const { isLoggedIn } = useOutletContext<OuletContextType>();
 
-    return (
-        <>
-            <h1>Home page</h1>
-            {isLoggedIn ? <LoggedInHome /> : <LoggedOutHome />}
-        </>
-    )
+  const [scrollX, setScrollX] = useState(0);
+
+  // 스크롤 감지 핸들러
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollX(window.scrollY); // 스크롤 Y 값을 X 좌표 이동으로 사용
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <>
+      <Header scrollRatio={30} />
+      <HeaderTemp />
+      <div className="w-screen min-h-[calc(100vh-3.5rem)] primary-bg-black">
+        <div className="w-[1px] h-[400vh]"></div>
+      </div>
+      {/* <div className="login-div">
+          {isLoggedIn ? <LoggedInHome /> : <LoggedOutHome />}
+        </div>
+        <footer className="w-full"></footer> */}
+    </>
+  );
 }
 
 export default Home;
