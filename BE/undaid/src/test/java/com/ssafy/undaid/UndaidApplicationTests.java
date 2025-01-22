4d28a6a8f006538b97b481d33689a0fbbde10626
이유리 <yetcome0123@gmail.com>
2025-01-22 18:02:09 +0900

    build: 초기 세팅

---
+++ BE/undaid/src/test/java/com/ssafy/undaid/UndaidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.undaid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UndaidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
