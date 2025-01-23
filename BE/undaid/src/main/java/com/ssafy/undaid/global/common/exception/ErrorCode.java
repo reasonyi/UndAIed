ba5a845986141b60f6daeb097cb7fa3437f9c936
이유리 <yetcome0123@gmail.com>
2025-01-23 16:12:24 +0900

    style: response 형식 정의
    http 요청에 응답 시 일관적인 response를 제공할 수 있도록 형식을 정의함.
    git 꼬임 이슈로 다시 push

---
+++ BE/undaid/src/main/java/com/ssafy/undaid/global/common/exception/ErrorCode.java
@@ -0,0 +1,25 @@
+package com.ssafy.undaid.global.common.exception;
+
+import com.ssafy.undaid.global.common.response.HttpStatusCode;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+
+    // Global Exception
+    BAD_REQUEST_ERROR(HttpStatusCode.BAD_REQUEST.getStatus(), "잘못된 요청입니다."),
+    INVALID_HTTP_MESSAGE_BODY(HttpStatusCode.BAD_REQUEST.getStatus(),"HTTP 요청 바디의 형식이 잘못되었습니다."),
+    UNSUPPORTED_HTTP_METHOD(HttpStatusCode.METHOD_NOT_ALLOWED.getStatus(),"지원하지 않는 HTTP 메서드입니다."),
+    SERVER_ERROR(HttpStatusCode.INTERNAL_SERVER_ERROR.getStatus(),"서버 내부에서 알 수 없는 오류가 발생했습니다."),
+
+    // Business Exception
+    NOT_AUTHENTICATED(HttpStatusCode.UNAUTHORIZED.getStatus(), "등록된 인증 정보가 없습니다."),
+
+    MEMBER_NOT_FOUND(HttpStatusCode.BAD_REQUEST.getStatus(), "유저를 찾을 수 없습니다.")
+    ;
+
+    private final int httpStatus;
+    private final String message;
+}
---
+++ BE/undaid/src/main/java/com/ssafy/undaid/global/common/response/ApiResponse.java
@@ -0,0 +1,33 @@
+package com.ssafy.undaid.global.common.response;
+
+import com.ssafy.undaid.global.common.exception.ErrorCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@ToString
+@RequiredArgsConstructor
+@Getter
+public class ApiResponse {
+
+    private final String timeStamp = LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME);
+    private final Boolean isSuccess;
+    private final int status;
+    private final String message;
+
+
+    public static ApiResponse of(HttpStatusCode statusCodes) {
+        return new ApiResponse(statusCodes.getIsSuccess(), statusCodes.getStatus(), null);
+    }
+
+    public static ApiResponse of(HttpStatusCode statusCodes, String message) {
+        return new ApiResponse(statusCodes.getIsSuccess(), statusCodes.getStatus(), message);
+    }
+
+    public static ApiResponse of(ErrorCode errorCode) {
+        return new ApiResponse(false, errorCode.getHttpStatus(), errorCode.getMessage());
+    }
+}
