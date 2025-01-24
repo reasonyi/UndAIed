6387065ab40a30275acdea143e9fe17414de7d00
이유리 <yetcome0123@gmail.com>
2025-01-24 23:12:08 +0900

    feat: jwt를 이용하여 사용자 인증 가능하도록 구현
    accessToken과 refreshToken을 사용하여 사용자 로그인 관리

---
+++ BE/undaid/src/main/java/com/ssafy/undaid/global/jwt/RefreshTokenRepository.java
@@ -0,0 +1,8 @@
+package com.ssafy.undaid.global.jwt;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
+}
