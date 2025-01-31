a6a51d8cb5feef391569571e316954e548802223
choihyunman <cchh6462@naver.com>
2025-01-31 16:30:01 +0900

    feat:게시판, 댓글 crud 기능 구현

---
+++ BE/undaid/src/main/java/com/ssafy/undaid/domain/comment/dto/request/CommentCreateRequestDto.java
@@ -0,0 +1,10 @@
+package com.ssafy.undaid.domain.comment.dto.request;
+
+import lombok.Getter;
+
+@Getter
+public class CommentCreateRequestDto {
+
+    private String commentContent;
+
+}
---
+++ BE/undaid/src/main/java/com/ssafy/undaid/domain/comment/dto/request/CommentUpdateRequestDto.java
@@ -0,0 +1,8 @@
+package com.ssafy.undaid.domain.comment.dto.request;
+
+import lombok.Getter;
+
+@Getter
+public class CommentUpdateRequestDto {
+    private String commentContent;
+}
