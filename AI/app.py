ca99bf3985122fe6566dc459631a83e314749747
enderstream00 <enderstream00@naver.com>
2025-02-03 03:25:53 +0900

    test: docker-compose 전환 및 프록시서버 세팅 완료

--- AI/app.py
+++ AI/app.py
@@ -6,11 +6,13 @@ app = FastAPI()
 # env = dotenv_values(".env")
 # print(env["OPENAI_API_KEY"])
 
-@app.get("/")
+@app.get("/api/ai/")  # "/" -> "/api/ai/"로 변경
 def read_root():
     return {"Hello": "World"}
 
-
-@app.get("/items/{item_id}")
+@app.get("/api/ai/items/{item_id}")  # "/items/" -> "/api/ai/items/"로 변경
 def read_item(item_id: int, q: str | None = None):
     return {"item_id": item_id, "q": q}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
