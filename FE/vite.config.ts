f759542022972bffebee393c22db84a5391c7201
enderstream00 <enderstream00@naver.com>
2025-02-04 10:25:31 +0900

    Revert: 97a0224시점으로 돌아감:: "Merge branch 'test' into 'develop'"
    This reverts commit 97a0224d6625d5cb8f65821bc76aba035de024c0, reversing
    changes made to 4c80a6cdcfbc9e8ed8f75190fcfec51e5a404e7c.

--- FE/vite.config.ts
+++ FE/vite.config.ts
@@ -1,21 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true,
-    port: 5173,
-    watch: {
-      usePolling: true
-    },
-    cors: true,
-    strictPort: true,
-    hmr: {
-      clientPort: 5173
-    },
-    // 명시적으로 허용할 호스트 추가
-    allowedHosts: ['i12b212.p.ssafy.io', 'localhost']
-  }
 })
