32657337e8e8c537e3bbe738173928a91b0b149b
이유리 <yetcome0123@gmail.com>
2025-02-05 16:59:02 +0900

    refactor: 패키지 구조 변경

---
+++ BE/undaid/src/main/java/com/ssafy/undaied/socket/common/constant/SocketRoom.java
@@ -0,0 +1,6 @@
+package com.ssafy.undaied.socket.common.constant;
+
+public class SocketRoom {
+    public static final String LOBBY_ROOM = "lobby";
+    public static final String GAME_ROOM_PREFIX = "game-room-";  // 게임방 접두사
+}
