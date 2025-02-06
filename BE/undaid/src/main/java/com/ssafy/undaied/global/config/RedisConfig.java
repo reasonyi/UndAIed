754568bd1f8815e66c3ebf68110c859749f1d9b5
이유리 <yetcome0123@gmail.com>
2025-02-06 17:33:16 +0900

    feat: socketIo에서 한번에 처리하던 책임을 분리

--- BE/undaid/src/main/java/com/ssafy/undaied/global/config/RedisConfig.java
+++ BE/undaid/src/main/java/com/ssafy/undaied/global/config/RedisConfig.java
@@ -1,44 +1,27 @@
 package com.ssafy.undaied.global.config;
 
-<<<<<<< HEAD
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-=======
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.PropertyAccessor;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.ssafy.undaied.socket.room.dto.Room;
-import jakarta.annotation.PostConstruct;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
->>>>>>> d7572cc (feat: 소켓 데이터 json 직렬화 설정)
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import java.time.LocalDateTime;
 
 @EnableRedisRepositories
 @Configuration
-@Slf4j
 public class RedisConfig {
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
-        configuration.setHostName(host);
-        configuration.setPort(port);
-        return new LettuceConnectionFactory(configuration);
+        return new LettuceConnectionFactory();
     }
 
-<<<<<<< HEAD
     // ✅ 문자열 저장 전용 (기존 redisTemplate)
     @Bean
     public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
@@ -69,7 +52,6 @@ public class RedisConfig {
 
         template.setKeySerializer(new StringRedisSerializer());
         template.setHashKeySerializer(new StringRedisSerializer());
-
         template.setValueSerializer(serializer);
         template.setHashValueSerializer(serializer);
 
