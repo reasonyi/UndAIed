7abdff535ce93c69efa4847e15d7a9f5b628ec5a
이유리 <yetcome0123@gmail.com>
2025-02-06 17:32:23 +0900

    feat: 로비 입장 및 퇴장 기능 구현

---
+++ BE/undaid/src/main/java/com/ssafy/undaied/socket/lobby/service/LobbyService.java
@@ -0,0 +1,40 @@
+package com.ssafy.undaied.socket.lobby.service;
+
+import com.corundumstudio.socketio.SocketIOClient;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+import static com.ssafy.undaied.socket.common.constant.SocketRoom.LOBBY_ROOM;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class LobbyService {
+    /**
+     * 클라이언트를 로비에 입장시킵니다.
+     */
+    public void joinLobby(SocketIOClient client, int userId) {
+        client.joinRoom(LOBBY_ROOM);
+        log.info("User {} (sessionId: {}) joined lobby", userId, client.getSessionId());
+    }
+
+    /**
+     * 클라이언트를 로비에서 퇴장시킵니다.
+     */
+    public void leaveLobby(SocketIOClient client) {
+        client.leaveRoom(LOBBY_ROOM);
+        log.info("User {} (sessionId: {}) left lobby", client.get("userId"), client.getSessionId());
+    }
+
+    public boolean isUserInLobby(SocketIOClient client) {
+        Set<String> rooms = client.getAllRooms();
+        if (rooms.size() != 1 || !rooms.contains(LOBBY_ROOM)) {
+            return false;
+        }
+        return true;
+    }
+
+}
