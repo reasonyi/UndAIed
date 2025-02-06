496262b859736ee6640ec69b4d232c9ef99b4e82
이유리 <yetcome0123@gmail.com>
2025-02-06 17:37:42 +0900

    feat: redis 저장용 접두사 추가 및 수정

--- BE/undaid/src/main/java/com/ssafy/undaied/socket/common/constant/SocketRoom.java
+++ BE/undaid/src/main/java/com/ssafy/undaied/socket/common/constant/SocketRoom.java
@@ -2,5 +2,6 @@ package com.ssafy.undaied.socket.common.constant;
 
 public class SocketRoom {
     public static final String LOBBY_ROOM = "lobby";
-    public static final String GAME_ROOM_PREFIX = "game-room-";  // 게임방 접두사
+    public static final String ROOM_KEY_PREFIX = "room:";  // 게임방 접두사
+    public static final String ROOM_SEQUENCE_KEY = "room:sequence";
 }
