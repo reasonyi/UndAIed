77fd48d786c3a77ed20296d3f19b6ac08c0e8324
sunny <rjqnrdl5113@naver.com>
2025-02-08 00:00:59 +0900

    feat: getCurrentStage 메서드 추가
    - 현재 stage 확인을 위한getCurrentStage 메서드 추가
    - CHAT_GAME EventType 통일

--- BE/undaid/src/main/java/com/ssafy/undaied/socket/common/constant/EventType.java
+++ BE/undaid/src/main/java/com/ssafy/undaied/socket/common/constant/EventType.java
@@ -3,7 +3,6 @@ package com.ssafy.undaied.socket.common.constant;
 public enum EventType {
 
     // 로비
-    CREATE_ROOM_AT_LOBBY("lobby:room:create"),
     CREATE_ROOM("room:create"),
     LIST_ROOMS("room:list"),
     ENTER_ROOM("room:enter"),
@@ -17,24 +16,15 @@ public enum EventType {
 
     // 게임진행(낮)
     START_SUBJECT_DEBATE("debate:subject:start"),
-    SEND_SUBJECT_DEBATE("debate:subject:send"),
-    END_SUBJECT_DEBATE("debate:subject:end"),
     START_FREE_DEBATE("debate:free:start"),
-    SEND_FREE_DEBATE("debate:free:send"),
-    END_FREE_DEBATE("debate:free:end"),
+    GAME_CHAT("chat:game"),
 
     // 투표
-    START_VOTE("vote:start"),
-    END_VOTE("vote:end"),
     SUBMIT_VOTE ("vote:submit"),
     SHOW_VOTE_RESULT("vote:result"),
 
     // 게임 진행 밤
     SHOW_INFECTION_RESULT("infection:result"),
-
-    // 스테이지 알림
-    START_STAGE("game:stage:start"),
-    END_STAGE("game:stage:end"),
 
     // 게임 종료
     SHOW_GAME_RESULT("game:result"),
