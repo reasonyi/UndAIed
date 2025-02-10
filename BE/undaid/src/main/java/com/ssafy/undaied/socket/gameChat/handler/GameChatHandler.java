b4086e66cf2221ecabc775424f8f8f61986554e7
sunny <rjqnrdl5113@naver.com>
2025-02-10 13:29:47 +0900

    modify: StageHandler 라운드 알림 추가

--- BE/undaid/src/main/java/com/ssafy/undaied/socket/gameChat/handler/GameChatHandler.java
+++ BE/undaid/src/main/java/com/ssafy/undaied/socket/gameChat/handler/GameChatHandler.java
@@ -4,7 +4,7 @@ import com.corundumstudio.socketio.SocketIOClient;
 import com.corundumstudio.socketio.annotation.OnEvent;
 import com.ssafy.undaied.socket.gameChat.dto.request.GameChatRequestDto;
 import com.ssafy.undaied.socket.gameChat.service.GameChatService;
-import com.ssafy.undaied.socket.gameStage.handler.GameStageHandler;
+import com.ssafy.undaied.socket.stage.handler.StageHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Component;
 public class GameChatHandler {
 
     private final GameChatService gameChatService;
-    private final GameStageHandler gameStageHandler;
+    private final StageHandler stageHandler;
 
     @OnEvent("chat:game")
     public void GameChat(SocketIOClient client, GameChatRequestDto gameChatRequestDto) {
