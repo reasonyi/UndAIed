b93a33dd5c17f82f9dadd69bafee5664e7f6e6b9
kub938 <kub938@naver.com>
2025-02-10 22:49:59 +0900

    feat: sidebar 오류 처리 로직 변경

--- FE/src/pages/GameMain/components/GameSidebar.tsx
+++ FE/src/pages/GameMain/components/GameSidebar.tsx
@@ -10,7 +10,7 @@ import { useUserProfile } from "../../../hooks/useUserData";
 
 function GameSidebar() {
   const blockStyle =
-    "border border-[#f74a5c]/60 backdrop-blur-[12.20px] text-[#fffbfb]  rounded-[5px]  transition-all duration-200 ";
+    "border border-[#f74a5c]/60 backdrop-blur-[12.20px] text-[#fffbfb] rounded-[5px] transition-all duration-200";
   const blockHover =
     "hover:bg-[#f8376467] hover:border-[#f93c4f] hover:shadow-[0_0_15px_0] hover:shadow-[#F74A5C]";
   const blockActive =
@@ -18,51 +18,55 @@ function GameSidebar() {
 
   const { data, isLoading, error } = useUserProfile();
 
-  if (isLoading) {
-    return <div>로딩중 입니다.</div>;
-  }
-  if (error) {
-    console.log("유저 프로필 호출 실패", error);
-    return <div>유저 프로필 호출 실패</div>;
-  }
   return (
     <aside
-      className={` mx-8 w-80 ${blockStyle}  flex-col items-center  bg-[#00000040] hidden md:flex`}
+      className={`mx-8 w-80 ${blockStyle} flex-col items-center bg-[#00000040] hidden md:flex`}
     >
-      <div className="flex align-middle">
-        <div className="w-32 h-32 mt-8 mb-3 flex items-center justify-center border border-[#f74a5c]/60">
-          <img src={`${playerIcon}`} alt="" />
+      {isLoading ? (
+        <div className="flex items-center justify-center h-full">
+          로딩중 입니다.
         </div>
-        <div className="w-32 mt-5 flex flex-col gap-2 items-center justify-center text-[#fcfafa]">
-          <div>{data.nickname} 임시 닉네임</div>
-          <div>총 판수 : {data.totalWin + data.totalLose} </div>
+      ) : error ? (
+        <div className="flex flex-col items-center justify-center h-full">
+          <div>프로필 로드 실패</div>
+          <div className="text-sm text-red-400">다시 시도해주세요</div>
         </div>
-      </div>
+      ) : (
+        <>
+          <div className="flex align-middle">
+            <div className="w-32 h-32 mt-8 mb-3 flex items-center justify-center border border-[#f74a5c]/60">
+              <img src={`${playerIcon}`} alt="" />
+            </div>
+            <div className="w-32 mt-5 flex flex-col gap-2 items-center justify-center text-[#fcfafa]">
+              <div>{data.nickname}</div>
+              <div>총 판수 : {data.totalWin + data.totalLose}</div>
+            </div>
+          </div>
 
-      <div className="mt-62 flex flex-col items-center">
-        <div className="mt-10 mb-7 text-2xl">
-          <DonutChart />
-        </div>
-        <div className="mb-7">
-          승률 | {data.totalWin} 승 | {data.totalLose} 패 |{" "}
-          {" " +
-            Math.round(
-              (data.totalWin / (data.totalLose + data.totalWin)) * 10000
-            ) /
-              100}
-          %
-        </div>
-      </div>
+          <div className="mt-62 flex flex-col items-center">
+            <div className="mt-10 mb-7 text-2xl">
+              <DonutChart />
+            </div>
+            <div className="mb-7">
+              승률 | {data.totalWin} 승 | {data.totalLose} 패 |{" "}
+              {Math.round(
+                (data.totalWin / (data.totalLose + data.totalWin)) * 10000
+              ) / 100}
+              %
+            </div>
+          </div>
 
-      <div className="mt-auto w-full flex flex-col items-center mb-8">
-        <Link to="/gamemypage">
-          <button
-            className={`${blockStyle} ${blockHover} ${blockActive} w-72 h-10 mt-4  bg-[#281919]`}
-          >
-            내 정보
-          </button>
-        </Link>
-      </div>
+          <div className="mt-auto w-full flex flex-col items-center mb-8">
+            <Link to="/gamemypage">
+              <button
+                className={`${blockStyle} ${blockHover} ${blockActive} w-72 h-10 mt-4 bg-[#281919]`}
+              >
+                내 정보
+              </button>
+            </Link>
+          </div>
+        </>
+      )}
     </aside>
   );
 }
