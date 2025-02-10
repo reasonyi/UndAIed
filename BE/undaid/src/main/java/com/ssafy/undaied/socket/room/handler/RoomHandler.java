90a3ebf2e2a86659c264bfbb0773dfbfffd7af8c
이유리 <yetcome0123@gmail.com>
2025-02-10 15:39:10 +0900

    feat: 방 생성 이벤트를 ack로 받아 응답보내도록 변경

--- BE/undaid/src/main/java/com/ssafy/undaied/socket/room/handler/RoomHandler.java
+++ BE/undaid/src/main/java/com/ssafy/undaied/socket/room/handler/RoomHandler.java
@@ -3,6 +3,8 @@ package com.ssafy.undaied.socket.room.handler;
 import com.corundumstudio.socketio.SocketIOClient;
 import com.corundumstudio.socketio.SocketIOServer;
 import com.ssafy.undaied.socket.common.exception.SocketException;
+import com.ssafy.undaied.socket.lobby.dto.response.RoomCreatedAtLobbyReponseDto;
+import com.ssafy.undaied.socket.lobby.service.LobbyService;
 import com.ssafy.undaied.socket.room.dto.Room;
 import com.ssafy.undaied.socket.room.dto.request.RoomCreateRequestDto;
 import com.ssafy.undaied.socket.room.dto.request.RoomEnterRequestDto;
@@ -15,6 +17,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import static com.ssafy.undaied.socket.common.constant.EventType.*;
+import static com.ssafy.undaied.socket.common.constant.SocketRoom.LOBBY_ROOM;
 import static com.ssafy.undaied.socket.common.constant.SocketRoom.ROOM_KEY_PREFIX;
 
 import com.ssafy.undaied.socket.common.exception.SocketErrorCode;
@@ -31,28 +34,36 @@ public class RoomHandler {
 
     private final SocketIOServer server;
     private final RoomService roomService;
+    private final LobbyService lobbyService;
 
     @PostConstruct
     private void init() {
         server.addEventListener(CREATE_ROOM_AT_LOBBY.getValue(), RoomCreateRequestDto.class,
                 (client, data, ackRequest) -> {
                     try {
+
                         // 방 생성.
                         RoomCreateResponseDto responseRoomData = roomService.createRoom(data, client);
                         log.info("Room created - ID: {}, Title: {}", responseRoomData.getRoomId(), responseRoomData.getRoomTitle());
-                        client.sendEvent(CREATE_ROOM.getValue(), responseRoomData); // 방을 생성한 클라이언트에게 데이터 전송
 
-                        ////                        // 로비에 있는 사람들에게 방 생성 알림
-                        ////                        server.getRoomOperations("lobby")
-                        ////                                .sendEvent(SocketEvent.ROOM_CREATED.getValue(), responseRoomData);
+
+                        // 방을 생성한 클라이언트에게 데이터 전송
+                        if (ackRequest.isAckRequested()) {
+                            Map<String, Object> response = new HashMap<>();
+                            response.put("success", true);
+                            response.put("errorMessage", null);
+                            response.put("data", responseRoomData.getRoomId());
+                            ackRequest.sendAckData(response);
+                        }
+
                     } catch (Exception e) {
-                        log.error("Room creation failed: {}", e.getMessage());
-                        // connection을 끊지 않고 error 이벤트 emit
-                        Map<String, Object> errorData = new HashMap<>();
-                        errorData.put("code", CREATE_ROOM_FAILED.getStatus());
-                        errorData.put("message", CREATE_ROOM_FAILED.getMessage());
-                        client.sendEvent("error", errorData);
-                        // throw하지 않음으로써 connection 유지
+                        if (ackRequest.isAckRequested()) {
+                            Map<String, Object> response = new HashMap<>();
+                            response.put("isError", true);
+                            response.put("errorMessage", e.getMessage());
+                            response.put("data", null);
+                            ackRequest.sendAckData(response);
+                        }
                     }
                 }
         );
@@ -71,10 +82,9 @@ public class RoomHandler {
                 }
         );
 
-        server.addEventListener(ENTER_ROOM_AT_LOBBY.getValue(), RoomEnterRequestDto.class,
+        server.addEventListener(ENTER_ROOM.getValue(), RoomEnterRequestDto.class,
                 (client, data, ackRequest) -> {
                     try {
-                        System.out.println("여기 들어올까");
                         roomService.enterRoom(client, data.getRoomId(), data.getRoomPassword());
                     } catch (Exception e) {
                         log.error("Room enter failed: {}", e.getMessage());
