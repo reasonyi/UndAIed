a482afcedb06322466e6463e1fc83681f820a98f
kub938 <kub938@naver.com>
2025-02-10 23:56:31 +0900

    feat: api 주소 변경, user response data 파싱 후 적용용

--- FE/src/api/userDataApi.ts
+++ FE/src/api/userDataApi.ts
@@ -2,15 +2,19 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
 export const userApi = axios.create({
-  baseURL: "http://localhost5173.com",
+  baseURL: "http://localhost:8080",
 });
 
 userApi.interceptors.request.use(
   //header에 user token 넣어서 api 호출
   (config) => {
-    const token = localStorage.getItem("userPersist");
-    if (token) {
-      config.headers.Authorization = `Brarer ${token}`;
+    const userPersist = sessionStorage.getItem("userPersist");
+    if (userPersist) {
+      const { userState } = JSON.parse(userPersist);
+      const token = userState.token; // JSON에서 token만 추출
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
     }
     return config;
   },
