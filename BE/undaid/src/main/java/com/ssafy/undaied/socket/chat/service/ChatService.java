c3226840f4d346675f5565ed9674a173e0d4afe7
이유리 <yetcome0123@gmail.com>
2025-02-11 13:33:18 +0900

    feat: 로비 채팅 구현

--- BE/undaid/src/main/java/com/ssafy/undaied/socket/chat/service/ChatService.java
+++ BE/undaid/src/main/java/com/ssafy/undaied/socket/chat/service/ChatService.java
@@ -2,6 +2,7 @@ package com.ssafy.undaied.socket.chat.service;
 
 import com.corundumstudio.socketio.SocketIOClient;
 import com.corundumstudio.socketio.SocketIOServer;
+import com.ssafy.undaied.socket.chat.dto.request.LobbyChatRequestDto;
 import com.ssafy.undaied.socket.chat.dto.request.RoomChatRequestDto;
 import com.ssafy.undaied.socket.chat.dto.response.RoomChatResponseDto;
 import com.ssafy.undaied.socket.common.exception.SocketException;
@@ -9,8 +10,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import static com.ssafy.undaied.socket.common.constant.EventType.LEAVE_ROOM;
-import static com.ssafy.undaied.socket.common.constant.EventType.ROOM_CHAT;
+import static com.ssafy.undaied.socket.common.constant.EventType.*;
 import static com.ssafy.undaied.socket.common.constant.SocketRoom.*;
 import static com.ssafy.undaied.socket.common.exception.SocketErrorCode.*;
 
@@ -35,6 +35,20 @@ public class ChatService {
 
         server.getRoomOperations(key).sendEvent(ROOM_CHAT.getValue(), roomChat);
 
+    }
+
+    public void lobbyChat(LobbyChatRequestDto requestDto, SocketIOClient client) throws SocketException {
+        if(requestDto == null || client == null) {
+            log.info("request data null");
+            throw new SocketException(LOBBY_CHAT_FAILED);
+        }
+
+        RoomChatResponseDto loobyChat = RoomChatResponseDto.builder()
+                .nickname(client.get("nickname"))
+                .message(requestDto.getMessage())
+                .build();
+
+        server.getRoomOperations(LOBBY_ROOM).sendEvent(LOBBY_CHAT.getValue(), loobyChat);
     }
 
 
