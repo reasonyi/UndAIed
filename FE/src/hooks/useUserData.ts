5b1d326ae1ce0f94c885ea273512a11d47cf282f
kub938 <kub938@naver.com>
2025-02-13 00:48:09 +0900

    refactor: apiClient로 변경

--- FE/src/hooks/useUserData.ts
+++ FE/src/hooks/useUserData.ts
@@ -12,29 +12,28 @@
 //   });
 // }
 
-
 // useUserData.ts
 import { useQuery } from "@tanstack/react-query";
-import { userApi } from "../api/userDataApi";
+import { apiClient } from "../api/apiClient";
 import { useRecoilValue } from "recoil";
 import { userState } from "../store/userState";
 
 export function useUserProfile() {
- const user = useRecoilValue(userState);
- 
- return useQuery({
-   queryKey: ["userProfile"],
-   queryFn: async () => {
-     const response = await userApi.get("api/v1/user/profile", {
-       headers: {
-         Authorization: `Bearer ${user.token}`
-       }
-     });
-     console.log("유저 데이터 수신 완료", response);
-     return response.data;
-   },
-   enabled: !!user.token, // 토큰이 있을 때만 쿼리 실행
-   retry: 1,
-   refetchOnWindowFocus: false
- });
+  const user = useRecoilValue(userState);
+
+  return useQuery({
+    queryKey: ["userProfile"],
+    queryFn: async () => {
+      const response = await apiClient.get("api/v1/user/profile", {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      });
+      console.log("유저 데이터 수신 완료", response);
+      return response.data;
+    },
+    enabled: !!user.token, // 토큰이 있을 때만 쿼리 실행
+    retry: 1,
+    refetchOnWindowFocus: false,
+  });
 }
