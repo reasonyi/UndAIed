b6ec6a6d75c9dd0c81c748df9af73a26be1ab441
enderstream00 <enderstream00@naver.com>
2025-02-12 10:21:55 +0900

    fix: 소켓 문제 해결결

--- FE/src/pages/Game.tsx
+++ FE/src/pages/Game.tsx
@@ -1,23 +1,23 @@
 import { Outlet } from "react-router";
 import { SocketProvider } from "../components/SocketContext";
 
-function Game() {
-  return (
-    <>
-        <Outlet />
-    </>
-  );
-}
-
-export default Game;
 // function Game() {
 //   return (
 //     <>
-//       <SocketProvider>
 //         <Outlet />
-//       </SocketProvider>
 //     </>
 //   );
 // }
 
 // export default Game;
+function Game() {
+  return (
+    <>
+      <SocketProvider url="https://i12b212.p.ssafy.io">
+        <Outlet />
+      </SocketProvider>
+    </>
+  );
+}
+
+export default Game;
