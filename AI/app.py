62e25ef15ada5daa1203adfcb39652840beef989
enderstream00 <enderstream00@naver.com>
2025-02-13 17:59:51 +0900

    feat: 수정 중

--- AI/app.py
+++ AI/app.py
@@ -1,15 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 from models.Gemini import init_genimi
 from datetime import datetime
+from pydantic import BaseModel
 
 
 app = FastAPI()
 gemini_bot = init_genimi()
 
+class messageRequest(BaseModel):
+    message:str
+
 
 @app.post("/api/ai/{room_number}/")
-def create_message(room_number: int, message: dict):
-    print(message)
+def create_message(room_number: int, message: messageRequest):
+    print(message.message)
     # AI_message = gemini_bot.generate_content(message)
     AI_message = "hello!!!"
     precise_timestamp = datetime.now().isoformat()
