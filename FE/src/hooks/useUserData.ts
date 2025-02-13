f88b328a370687846d13040637528fdc57f279af
kub938 <kub938@naver.com>
2025-02-13 13:43:49 +0900

    refactor: aliClient 훅 적용

--- FE/src/hooks/useUserData.ts
+++ FE/src/hooks/useUserData.ts
@@ -1,22 +1,9 @@
-// import { useQuery } from "@tanstack/react-query";
-// import { userApi } from "../api/userDataApi";
-
-// export function useUserProfile() {
-//   return useQuery({
-//     queryKey: ["userProfile"],
-//     queryFn: async () => {
-//       const response = await userApi.get("api/v1/user/profile");
-//       console.log("유저 데이터 수신 완료", response);
-//       return response.data;
-//     },
-//   });
-// }
-
-// useUserData.ts
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { apiClient } from "../api/apiClient";
 import { useRecoilValue } from "recoil";
 import { userState } from "../store/userState";
+import { updateProfile } from "../api/userDataApi";
+import { toast } from "sonner";
 
 export function useUserProfile() {
   const user = useRecoilValue(userState);
@@ -24,11 +11,7 @@ export function useUserProfile() {
   return useQuery({
     queryKey: ["userProfile"],
     queryFn: async () => {
-      const response = await apiClient.get("api/v1/user/profile", {
-        headers: {
-          Authorization: `Bearer ${user.token}`,
-        },
-      });
+      const response = await apiClient.get("api/v1/user/profile");
       console.log("유저 데이터 수신 완료", response);
       return response.data;
     },
@@ -37,3 +20,19 @@ export function useUserProfile() {
     refetchOnWindowFocus: false,
   });
 }
+
+export const useUpdateProfile = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: updateProfile,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["userProfile"] });
+      toast.success("프로필이 성공적으로 업데이트 되었습니다.");
+    },
+    onError: (error) => {
+      toast.error("프로필 업데이트에 실패했습니다.");
+      console.error("프로필 업데이트 오류: ", error);
+    },
+  });
+};
