0ba55da4f9930a8318c8238144313a323754a6ff
enderstream00 <enderstream00@naver.com>
2025-02-16 02:12:21 +0900

    feat: AI서버 구축, gemini 챗봇 완성, 파싱 알고리즘 구현 완료

--- AI/models/Gemini.py
+++ AI/models/Gemini.py
@@ -1,24 +1,37 @@
 import google.generativeai as genai
 from environs import Env
-from prompt import prompt
+from .history import history
+from .GeminiNoHistoryChatSession import NoHistoryChatSession
+
 
 class GeminiBot:
     def __init__(self):
         env = Env()
         env.read_env()
-        GEMINI_API_KEY = env.str("GEMINI_API_KEY")
-        
-        genai.configure(api_key=GEMINI_API_KEY)
-        self.model = genai.GenerativeModel("gemini-2.0-flash-exp")
-        self.prompt = self.load_prompt()
+        genai.configure(api_key=env.str("GEMINI_API_KEY"))
 
-    def load_prompt(self):
-        return prompt
+        # 모델 초기화
+        self.model = genai.GenerativeModel(
+            "gemini-2.0-flash-exp",
+            generation_config={
+                "temperature": 1,
+                "top_p": 0.95,
+                "top_k": 40,
+                "max_output_tokens": 8192,
+                "response_mime_type": "text/plain",
+            },
+        )
 
-    def generate_response(self, user_input:dict):
-        full_prompt = f"{self.prompt}\n\n현재 게임 상황:\n{user_input}\n\n당신의 응답:"
-        response = self.model.generate_content(full_prompt)
+        # 커스텀 ChatSession으로 초기화
+        self.chat = NoHistoryChatSession(
+            model=self.model,
+            history=history,  # 시스템 프롬프트만 담김, 이전 대화 기억하지 않음음
+            enable_automatic_function_calling=False,
+        )
+
+    def generate_response(self, user_input: dict) -> str:
+        """채팅 세션 내에서 새로운 입력에 대한 응답을 생성합니다."""
+        response = self.chat.send_message(
+            f"현재 게임 상황:\n{user_input}\n\n당신의 응답:"
+        )
         return response.text
-
-def init_gemini():
-    return GeminiBot()
