276bf3dcdcce163e39bf58dc41746ffcb5c38588
kub938 <kub938@naver.com>
2025-02-16 22:22:04 +0900

    feat: game header 링크 연동

--- FE/src/pages/GameMain/components/GameHeader.tsx
+++ FE/src/pages/GameMain/components/GameHeader.tsx
@@ -1,9 +1,12 @@
+import { Link } from "react-router-dom";
 import power from "../../../assets/icon/power.png";
 import logo from "../../../assets/svg-icon/game_logo.svg";
 function GameHeader() {
   return (
     <header className="p-4 md:p-6  border-black flex justify-between items-center px-6">
-      <img src={logo} alt="로고" className="h-6" />
+      <Link to="/game">
+        <img src={logo} alt="로고" className="h-6" />
+      </Link>
       <button className="px-2 py-1 rounded-lg duration-300 hover:bg-[#f8376467] hover:border-[#f93c4f] hover:shadow-[#F74A5C]">
         <img src={power} alt="" />
       </button>
