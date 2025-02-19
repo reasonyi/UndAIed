e91b0f424c1eadaa2b8050b9b64574afa1dfa351
kub938 <kub938@naver.com>
2025-02-19 10:18:00 +0900

    refactor: ClientApi 적용

--- FE/src/api/boardApi.ts
+++ FE/src/api/boardApi.ts
@@ -1,50 +1,23 @@
-import axios from "axios";
 import { BoardRequest } from "../types/board";
-
-const api = axios.create({
-  baseURL: "/api/v1/board",
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
-
-const adminApi = axios.create({
-  baseURL: "/api/v1/admin/board",
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
-
-api.interceptors.request.use((config) => {
-  //요청 보내기전에 가로채서 header에 토큰 주입
-  const token = localStorage.getItem("token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
-
-adminApi.interceptors.request.use((config) => {
-  //요청 보내기전에 가로채서 header에 토큰 주입
-  const token = localStorage.getItem("token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
+import { apiClient } from "../api/apiClient";
 
 export const boardApi = {
-  getPosts: () => api.get(""),
-  getPost: (id: number) => api.get(`${id}`),
-  createPost: (data: BoardRequest) => api.post("", data),
-  updatePost: (id: number, data: Partial<BoardRequest>) => api.put(`${id}`),
-  deletePost: (id: number) => api.delete(`/${id}`),
+  getPosts: (category: number, page: number) =>
+    apiClient.get("/api/v1/board", {
+      params: { category, page: page - 1 },
+    }),
+  getPost: (id: number) => apiClient.get(`/api/v1/board/${id}`),
+  createPost: (data: BoardRequest) => apiClient.post("/api/v1/board", data),
+  updatePost: (id: number, data: Partial<BoardRequest>) =>
+    apiClient.put(`${id}`),
+  deletePost: (id: number) => apiClient.delete(`/${id}`),
 };
 
 export const AdminBoardApi = {
   //공지사항
-  createPost: (data: BoardRequest) => adminApi.post("", data),
+  createPost: (data: BoardRequest) =>
+    apiClient.post("/api/v1/admin/board", data),
   updatePost: (id: number, data: Partial<BoardRequest>) =>
-    api.put(`${id}`, data),
-  deletePost: (id: number) => api.delete(`/${id}`),
+    apiClient.put(`/api/v1/admin/board/${id}`, data),
+  deletePost: (id: number) => apiClient.delete(`/api/v1/admin/board/${id}`),
 };
