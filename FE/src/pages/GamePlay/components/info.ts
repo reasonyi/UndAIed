e434c339fc590d23255b048d89613637c4459198
imjh990614 <imjh990614@gmail.com>
2025-02-20 10:04:09 +0900

    modify: FE 시간 변경

--- FE/src/pages/GamePlay/components/info.ts
+++ FE/src/pages/GamePlay/components/info.ts
@@ -2,8 +2,8 @@
 export const STAGE_INFO = {
   start: ["게임 준비 중...", 0],
   day: ["라운드 준비 중...", 5],
-  subject_debate: ["주제 토론 시간", 15],
-  free_debate: ["자유 토론 시간", 10],
+  subject_debate: ["주제 토론 시간", 20],
+  free_debate: ["자유 토론 시간", 40],
   vote: ["투표 시간", 10],
   night: ["AI의 공격...", 5],
   finish: ["게임 종료", 0],
