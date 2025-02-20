ff0cf9621aecb3986b767a7ce76e872ad93a3b48
enderstream00 <enderstream00@naver.com>
2025-02-20 13:50:30 +0900

    fix: url hotfix

--- BE/undaid/src/main/java/com/ssafy/undaied/global/config/WebClientConfig.java
+++ BE/undaid/src/main/java/com/ssafy/undaied/global/config/WebClientConfig.java
@@ -1,14 +1,20 @@
 package com.ssafy.undaied.global.config;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class WebClientConfig {
+    @Value("${ai.server.url}")
+    private String aiHost;
     @Bean
     public WebClient webClient() {
+
+
         return WebClient.builder()
-            .baseUrl("http://localhost:8000")
+            .baseUrl(aiHost)
+            // .baseUrl("http://localhost:8000")
             .build();
     }
 }
