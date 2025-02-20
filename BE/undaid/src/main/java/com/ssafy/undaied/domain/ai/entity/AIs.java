51f59b1417610481f696184e3c3394db931e3256
이유리 <yetcome0123@gmail.com>
2025-02-20 10:27:56 +0900

    hotfix: ais pk 자동 increase, 날짜 자동 입력되도록 변경

--- BE/undaid/src/main/java/com/ssafy/undaied/domain/ai/entity/AIs.java
+++ BE/undaid/src/main/java/com/ssafy/undaied/domain/ai/entity/AIs.java
@@ -14,12 +14,12 @@ import java.time.LocalDateTime;
 public class AIs {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)  // AUTO_INCREMENT를 위한 설정
     private int aiId;
 
-    @Column(length = 50)
-    @NonNull
+    @Column(length = 50, nullable = false)
     private String aiName;
 
-    @NonNull
+    @Column(columnDefinition = "DATETIME DEFAULT CURRENT_TIMESTAMP")
     private LocalDateTime createdAt;
 }
