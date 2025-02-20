19ec580d19eead833550a4a0cb788137529ef0f0
enderstream00 <enderstream00@naver.com>
2025-02-21 00:45:56 +0900

    chore: 불필요한 주석 정리

--- FE/vite.config.ts
+++ FE/vite.config.ts
@@ -11,47 +11,7 @@ export default defineConfig({
     strictPort: true,
     port: 5173,
     cors: true,
-    // hmr: {
-    //   clientPort: 5173
-    // },
-    // hmr:false,
     // 명시적으로 허용할 호스트 추가
     allowedHosts: ['i12b212.p.ssafy.io', 'localhost']
   }
 })
-
-
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     host: true,
-//     port: 5173,
-//     watch: {
-//       usePolling: true
-//     },
-//     cors: true,
-//     strictPort: true,
-//     hmr: {
-//       protocol: 'wss',
-//       host: 'i12b212.p.ssafy.io',
-//       clientPort: 443
-//     },
-//     proxy: {
-//       '/api': {
-//         target: 'https://i12b212.p.ssafy.io',
-//         changeOrigin: true,
-//         secure: false
-//       },
-//       '/socket.io': {
-//         target: 'https://i12b212.p.ssafy.io',
-//         changeOrigin: true,
-//         ws: true,
-//         secure: false
-//       }
-//     },
-//     allowedHosts: ['i12b212.p.ssafy.io', 'localhost']
-//   }
-// })
